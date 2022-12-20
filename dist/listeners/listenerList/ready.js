6821921a43837e4c78d5af05bd99ba58c1a7f140
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-12-20 17:38:46 +0000

    Creamy

--- dist/listeners/listenerList/ready.js
+++ dist/listeners/listenerList/ready.js
@@ -5,7 +5,7 @@ var tslib_1 = require("tslib");
 var chalk_1 = (0, tslib_1.__importDefault)(require("chalk"));
 function ready(client) {
     client.on('ready', function () {
-        client.editStatus('idle', {
+        client.editStatus('online', {
             name: 'Daily amanzing news for Discord',
             type: 0,
         });
